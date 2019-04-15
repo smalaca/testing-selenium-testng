a1aea5aafeb4f45b7e06e548f3c47dd7927c75f9
smalaca <ab13.krakow@gmail.com>
2019-04-15 12:25:01 +0200

    parallel tests run

--- src/test/java/com/smalaca/pages/google/WebDriverTest.java
+++ src/test/java/com/smalaca/pages/google/WebDriverTest.java
@@ -5,23 +5,42 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class WebDriverTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void initDriver() {
+        driver = WebDriverFactory.driver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 
     @Test
     public void shouldCheckIfCanOpenGoogle() {
-        WebDriver driver = WebDriverFactory.driver();
-
         driver.get("http://www.google.com");
 
         List<WebElement> elements = driver.findElements(By.name("q"));
-        driver.findElement(By.name("q"));
 
         Assert.assertEquals(1, elements.size());
+    }
 
-        driver.close();
+    @Test
+    public void shouldCheckIfCanOpenCodeSprinters() {
+        WebDriver driver = WebDriverFactory.driver();
+
+        driver.get("http://agileszkolenia.pl");
+
+        List<WebElement> elements = driver.findElements(By.id("mce-FNAME"));
+
+        Assert.assertEquals(1, elements.size());
     }
 }
