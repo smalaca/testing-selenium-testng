56af9cd590684814981489e60a893ee9b4eaca63
smalaca <ab13.krakow@gmail.com>
2019-04-15 09:26:28 +0200

    project initialization

---
+++ src/main/java/com/smalaca/App.java
@@ -0,0 +1,7 @@
+package com.smalaca;
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/smalaca/WebDriverTest.java
@@ -0,0 +1,5 @@
+package com.smalaca;
+
+public class WebDriverTest {
+
+}
